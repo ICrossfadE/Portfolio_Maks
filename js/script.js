0dedb8619cbebe8323741e801f7c1ee3fde2baad
Maks <Crossfade143@gmail.com>
2023-01-26 20:23:37 +0200

    done section with client

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 3000,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+  });
+});
